1d02c55adfd29289fe6d4777c8310bef0e60ae3f
Evgeny Terechov <kiwi_studio@mail.ua>
2015-10-26 14:13:33 +0200

    change hardcode in monthjournal

---
+++ student/migrations/0011_auto_20151026_1108.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('student', '0010_auto_20150712_1439'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day1',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 1'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day10',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 10'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day11',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 11'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day12',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 12'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day13',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 13'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day14',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 14'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day15',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 15'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day16',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 16'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day17',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 17'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day18',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 18'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day19',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 19'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day2',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 2'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day20',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 20'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day21',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 21'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day22',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 22'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day23',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 23'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day24',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 24'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day25',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 25'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day26',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 26'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day27',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 27'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day28',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 28'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day29',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 29'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day3',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 3'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day30',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 30'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day31',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 31'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day4',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 4'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day5',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 5'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day6',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 6'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day7',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 7'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day8',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 8'),
+        ),
+        migrations.AlterField(
+            model_name='monthjournal',
+            name='present_day9',
+            field=models.BooleanField(default=False, verbose_name='\u0414\u0435\u043d\u044c 9'),
+        ),
+    ]
